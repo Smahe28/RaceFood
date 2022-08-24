82a4bc7cd11ff3781de37074518de02cbf44409c
Admin <Admin@DESKTOP-U2A2GFO>
2022-08-24 13:04:29 +0530

    Add project files.

---
+++ RaceFood/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RaceFood.Startup))]
+namespace RaceFood
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
